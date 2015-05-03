1a0da9d1baedacbce707554b76e91664a934a0e5
Andrew Brandon <drbr@users.noreply.github.com>
2015-05-02 17:14:36 -0700

    Sort the hand

--- tests/logic.js
+++ tests/logic.js
@@ -2,9 +2,30 @@ var expect = chai.expect;
 
 describe("Logic", function() {
 
-  describe("foo", function() {
-    it("should return bar", function() {
-      expect(Logic.foo()).to.equal("bar");
+  var aceOfClubs = { rank: "A", suit: "C" };
+  var twoOfHearts = { rank: "2", suit: "H" };
+  var sevenOfDiamonds = { rank: "7", suit: "D" };
+  var queenOfSpades = { rank: "Q", suit: "S" };
+  var kingOfSpades = { rank: "K", suit: "S" };
+  var kingOfDiamonds = { rank: "K", suit: "D" };
+
+  describe("cardRankComparator", function() {
+    it("should compare cards properly", function() {
+      expect(Logic.cardRankComparator(aceOfClubs, twoOfHearts)).to.be.below(0);
+      expect(Logic.cardRankComparator(queenOfSpades, twoOfHearts)).to.be.above(0);
+      expect(Logic.cardRankComparator(queenOfSpades, kingOfSpades)).to.be.below(0);
+      expect(Logic.cardRankComparator(sevenOfDiamonds, twoOfHearts)).to.be.above(0);
+      expect(Logic.cardRankComparator(kingOfSpades, kingOfDiamonds)).to.equal(0);
+    });
+  });
+
+  describe("scoreHand", function() {
+    it("should sort the hand correctly", function() {
+      var cards = [queenOfSpades, sevenOfDiamonds, aceOfClubs, kingOfDiamonds];
+      var starter = twoOfHearts;
+
+      var sortedHand = Logic.scoreHand(cards, starter);
+      expect(sortedHand).to.eql([aceOfClubs, twoOfHearts, sevenOfDiamonds, queenOfSpades, kingOfDiamonds]);
     });
   });
 });
