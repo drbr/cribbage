2f6e73fbe428994eebda247efabd38435765978f
Andrew Brandon <drbr@users.noreply.github.com>
2015-05-20 00:50:55 -0700

    Implemented crStarterScores with a nice table UI

--- src/app/angular/controllers/evaluateDealCtrl.js
+++ src/app/angular/controllers/evaluateDealCtrl.js
@@ -1,4 +1,5 @@
-crActivityControllers.controller('evaluateDealCtrl', ['LogicProvider', '$scope', function(LogicProvider, $scope) {
+crActivityControllers.controller('evaluateDealCtrl',
+    ['LogicProvider', 'UtilProvider', '$scope', function(LogicProvider, UtilProvider, $scope) {
   // These objects get populated by the card selectors
   // The card is wrapped in an object to avoid the isolate scope "dot problem".
   $scope.cards = Array.apply(null, new Array(6)).map(function() {
@@ -6,55 +7,53 @@ crActivityControllers.controller('evaluateDealCtrl', ['LogicProvider', '$scope',
   });
   $scope.cardsUnwrapped = [{}, {}, {}, {}, {}, {}];
 
+  // TODO: Temporary code to easily test
+  var TwoOfDiamonds = { rank: "2", suit: "D" };
+  var ThreeOfSpades = { rank: "3", suit: "S" };
+  var FiveOfClubs = { rank: "5", suit: "C" };
+  var FiveOfHearts = { rank: "5", suit: "H" };
+  var FiveOfSpades = { rank: "5", suit: "S" };
+  var FiveOfDiamonds = { rank: "5", suit: "D" };
+  $scope.cardsUnwrapped = [TwoOfDiamonds, ThreeOfSpades, FiveOfClubs, FiveOfHearts, FiveOfSpades, FiveOfDiamonds];
+  $scope.cards = $scope.cardsUnwrapped.map(function(card) {
+    return { card: card };
+  });
+
   $scope.setCard = function(newCard, index) {
     $scope.cards[index].card = newCard;
     $scope.cardsUnwrapped[index] = newCard;
   };
 
-  $scope.$watch('cards', function() {
-    $scope.breakdownsPerHand = breakdownsPerHand();
-    var breakdownObj = $scope.breakdownsPerHand[0];
-    $scope.scoreBreakdown = breakdownObj && breakdownObj.scoreBreakdownByStarter || 'Select cards first!';
-  }, true);
+  function setPromptForErrors() {
+    var duplicatesArray = LogicProvider.hands.findDuplicates($scope.cardsUnwrapped);
+    if (duplicatesArray.length > 0) {
+      var prompt = UtilProvider.viewUtils.stringifyDuplicateCards(duplicatesArray, LogicProvider.cards);
+      $scope.prompt = { prompt: prompt, classes: 'text-danger' };
+      return true;
+    }
 
-  // Returns an array of each possible four-card hand, sorted in descending order
-  // by its expected score (based on each possible starter card).
-  function breakdownsPerHand() {
-    var allCardsValid = $scope.cardsUnwrapped.every(function(card) {
+    var validCardSet = $scope.cardsUnwrapped.every(function(card) {
       return LogicProvider.cards.isValid(card);
     });
-
-    if (!allCardsValid) {
-      return [];
+    if (!validCardSet) {
+      $scope.prompt = { prompt: 'Select cards first!', classes: 'text-warning' };
+      return true;
     }
 
-    var breakdownsPerHand = [];
-    var possibleHands = LogicProvider.util.enumerateSubsets($scope.cardsUnwrapped, 4);
-    for (var i = 0; i < possibleHands.length; i++) {
-      var hand = possibleHands[i];
-
-      var starterScoresForHand = LogicProvider.hands.scoreForEachStarter(hand.in, hand.out);
-
-      var expectedScore = starterScoresForHand.map(function(scoreObj) {
-          return scoreObj.score;
-        }).reduce(function(sum, nextScore) {
-          return sum + nextScore;
-        }) / starterScoresForHand.length;
-
-      breakdownsPerHand.push({
-        fourCards: hand.in,
-        rejectedCards: hand.out,
-        expectedScore: expectedScore,
-        scoreBreakdownByStarter: starterScoresForHand
-      });
-    }
-
-    // Sort descending by the expected value of each 4-card hand
-    breakdownsPerHand.sort(function(a, b) {
-      return b.expectedScore - a.expectedScore;
-    });
-
-    return breakdownsPerHand;
+    $scope.prompt = null;
+    return false;
   };
+
+  $scope.$watch('cards', function() {
+    var cardsSelectedCorrectly = !setPromptForErrors();
+
+    if (cardsSelectedCorrectly) {
+      $scope.breakdownsPerHand = LogicProvider.hands.scoreBreakdownsPerHand($scope.cardsUnwrapped);
+
+      // TODO: Use the first one until the breakdown selector is built
+      var breakdownObj = $scope.breakdownsPerHand[0];
+      $scope.scoreBreakdown = breakdownObj.scoreBreakdownByStarter;
+    }
+  }, true);
 
 }]);
--- src/app/angular/directives/crStarterScores.js
+++ src/app/angular/directives/crStarterScores.js
@@ -1,12 +1,19 @@
 crDirectives.directive('crStarterScores',
-    ['LogicProvider', function(LogicProvider) {
+    ['LogicProvider', 'UtilProvider', function(LogicProvider, UtilProvider) {
   return {
     restrict: 'E',
     scope: {
       scoreBreakdown: '='
     },
     templateUrl: 'src/app/templates/crStarterScores.html',
-    link: function(scope, element, attrs) { return; }
+    link: function(scope, element, attrs) {
+      scope.$watch('scoreBreakdown', function() {
+        if (scope.scoreBreakdown && scope.scoreBreakdown.length) {
+          scope.tableRows = UtilProvider.viewUtils.
+            convertScoreBreakdownToTableRows(scope.scoreBreakdown, LogicProvider.cards);
+        }
+      });
+    }
   };
 
 }]);
